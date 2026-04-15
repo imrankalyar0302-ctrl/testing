ce53ec8f96e7894fec7f83065e9da9a2591e80e8
imrankalyar0302-ctrl <imrankalyar0302@gmail.com>
2026-04-15 11:02:34 +0500

    your comment

---
+++ home.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const home = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+      <Text>hi</Text>
+      <Text>hello</Text>
+    </View>
+  )
+}
+
+export default home
